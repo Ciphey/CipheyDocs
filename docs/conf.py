d402778bd880bad2c7a09a9390d66333d3f36773
Brandon <brandonskerritt51@gmail.com>
2020-07-09 22:28:45 +0100

    fixing readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,8 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx_autodoc_typehints",
 ]
+
+autodoc_mock_imports = ["ciphey"]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
